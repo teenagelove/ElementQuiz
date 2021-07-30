565dd8f2627106b5cbf5e39a50c757cb1944ebda
teenagelove <dani4upa586@gmail.com>
2021-07-30 22:06:21 +0500

    Create ElementQuiz.app

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,13 +7,201 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum Mode {
+    case flashCard
+    case quiz
 }
 
+enum State {
+    case question
+    case answer
+    case score
+}
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var showAnswerButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    var mode: Mode = .flashCard {
+        didSet {
+            switch mode {
+            case .flashCard:
+                setupFlashCards()
+            case .quiz:
+                setupQuiz()
+            }
+            updateUI()
+        }
+    }
+    var state: State = .question
+    let fixedElemetList = ["Carbon", "Gold", "Chlorine","Sodium"]
+    var elementList: [String] = []
+    var currentElementIndex = 0
+    var answerIsCorrect = false
+    var correctAnswerCount = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mode = .flashCard
+    }
+    
+    @IBAction func switchModes(_ sender: UISegmentedControl) {
+        if modeSelector.selectedSegmentIndex == 0 {
+            mode = .flashCard
+        } else {
+            mode = .quiz
+        }
+    }
+    
+    func updateFlashCardUI(elementName: String) {
+        textField.isHidden = true
+        textField.resignFirstResponder()
+        showAnswerButton.isHidden = false
+        nextButton.isEnabled = true
+        nextButton.setTitle("Next Element", for: .normal)
+        
+        if state == .answer {
+            answerLabel.text = elementName
+        } else {
+            answerLabel.text = "?"
+        }
+        
+        modeSelector.selectedSegmentIndex = 0
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        state = .answer
+        
+        updateUI()
+    }
+    
+    @IBAction func next(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+            if mode == .quiz {
+                state = .score
+                updateUI()
+                return
+            }
+                
+        }
+        
+        state = .question
+        
+        updateUI()
+    }
+    
+    func updateQuizUI(elementName: String) {
+        // Text field and keyboard
+        textField.isHidden = false
+        showAnswerButton.isHidden = true
+        
+        if currentElementIndex == elementList.count - 1 {
+            nextButton.setTitle("Show Score", for: .normal)
+        } else {
+            nextButton.setTitle("Next Question", for: .normal)
+        }
+        switch state {
+        case .question:
+            answerLabel.text = ""
+            textField.isEnabled = true
+            textField.becomeFirstResponder()
+            nextButton.isEnabled = false
+        case .answer:
+            textField.isEnabled = false
+            textField.resignFirstResponder()
+            nextButton.isEnabled = true
+        case .score:
+            textField.isHidden = true
+            textField.resignFirstResponder()
+            nextButton.isEnabled = false
+        }
+        
+        // Answer label
+        switch state {
+        case .question:
+            answerLabel.text = ""
+        case .answer:
+            if answerIsCorrect {
+                answerLabel.text = "Correct!"
+            } else {
+                answerLabel.text = "❌\nCorrect Aswer: " + elementName
+            }
+        case .score:
+            answerLabel.text = ""
+            print("Your score is \(correctAnswerCount) out of  \(elementList.count).")
+        }
+        
+        // Score display
+        if state == .score {
+            displayScoreAlert()
+        }
+        
+        modeSelector.selectedSegmentIndex = 1
+        
+    }
+    func updateUI() {
+        // Shared code: updating the image
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        switch mode {
+        case .flashCard:
+            updateFlashCardUI(elementName: elementName)
+        case .quiz:
+            updateQuizUI(elementName: elementName)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let textFieldContents = textField.text!
+        
+        if textFieldContents.lowercased() == elementList[currentElementIndex].lowercased() {
+            answerIsCorrect = true
+            correctAnswerCount += 1
+        } else {
+            answerIsCorrect = false
+        }
+        state = .answer
+        
+        updateUI()
+        
+        return true
+    }
+    
+    // Show an IOS alert with the user's quiz score.
+    func displayScoreAlert() {
+        let alert = UIAlertController(title: "Quiz Score", message: "Your score is \(correctAnswerCount) out of \(elementList.count).", preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "OK", style: .default, handler: scoreAlertDismissed(_:))
+        alert.addAction(dismissAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func scoreAlertDismissed(_ action: UIAlertAction) {
+        mode = .flashCard
+    }
+    
+    func setupFlashCards() {
+        elementList = fixedElemetList
+        state = .question
+        currentElementIndex = 0
+    }
+    
+    func setupQuiz() {
+        elementList = elementList.shuffled()
+        state = .question
+        currentElementIndex = 0
+        answerIsCorrect = false
+        correctAnswerCount = 0
+    }
+}
